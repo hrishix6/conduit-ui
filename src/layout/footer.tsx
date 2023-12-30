2c6055c7aa91730d55a6117894d46026d37a919b
hrishix6 <hrishi.pethe@gmail.com>
2023-12-30 15:09:47 +0530

    move github link to constants

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,10 +1,11 @@
 import githubLogo from "@/assets/github.svg";
+import { APP_GITHUB_LINK } from "@/constants";
 
 export default function Footer() {
   return (
     <footer className="fixed flex justify-center items-center bottom-0 left-0 w-full h-10 bg-secondary">
          <div className="flex items-center">
-            <a href="https://github.com/hrishix6/medium-clone-ui" target="_blank">
+            <a href={APP_GITHUB_LINK} target="_blank">
              <img src={githubLogo} className="h-6 w-6"  alt="github logo"/>
             </a>
          </div>
